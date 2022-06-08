2abd60d1e0b09506464bbc4a61a38deed43b6c17
Alekhoff <alekhoff@gmail.com>
2022-06-08 20:52:33 +0200

    changes

--- calc.js
+++ calc.js
@@ -1,9 +1,17 @@
+
 // Number used for converting
 let targetNumber = document.getElementById("convert-el");
+console.log(targetNumber);
+
 // Get class to display the same number as you are converting with
-let displayTargetNumb = document.getElementsByClassName("numb-targ");
-let converterNumber = 20; 
-console.log(targetNumber)
+let displayTargetNumb = document.getElementsByTagName("numb-targ");
+console.log(displayTargetNumb);
+
+targetNumber.textContent = 20;
+displayTargetNumb.textContent = 20;
+
+/* let converterNumber = 20; 
+console.log(targetNumber) */
 // displayTargetNumb.textContent = targetNumber;
 // targetNumber.textContent = converterNumber;
 // console.log(targetNumber);
