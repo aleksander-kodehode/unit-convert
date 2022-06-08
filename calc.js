0914a294b12046ec300617ab8475372174d315d2
Aleksander <aleksander.kodehode@gmail.com>
2022-06-08 20:15:10 +0200

    first commit

---
+++ calc.js
@@ -0,0 +1,9 @@
+// Number used for converting
+let targetNumber = document.getElementById("convert-el");
+// Get class to display the same number as you are converting with
+let displayTargetNumb = document.getElementsByClassName("numb-targ");
+let converterNumber = 20; 
+console.log(targetNumber)
+// displayTargetNumb.textContent = targetNumber;
+// targetNumber.textContent = converterNumber;
+// console.log(targetNumber);
